df2f8740f27f962fb1c270d44a434ffce967bed9
Igor 'c1tr00z' Nikitin <c1tr00z@yandex.ru>
2024-07-15 19:05:35 +0900

    improved tools

--- EditorTools/Scripts/AssistLibToolsPanel.cs
+++ EditorTools/Scripts/AssistLibToolsPanel.cs
@@ -4,40 +4,55 @@ using Godot;
 namespace projectwitch.addons.AssistLib.EditorTools.Scripts;
 
 [GlobalClass]
+[Tool]
 public partial class AssistLibToolsPanel : Control { //assist_lib_tools_panel_scene
 
     #region Private Fields
 
     private Node _toolsContainer;
 
+    private Control _toolsListNode;
+
+    private float _defaultPanelHeight = 900;
+
+    private float _panelHeightDelta = 0;
+
     #endregion
     
     #region Export Fields
 
     [Export] private NodePath _toolsContainerPath;
-
-    #endregion
     
-    #region Control Implementation
-    
-    public override void _EnterTree() {
-        base._EnterTree();
-        InitToolsPanels();
-    }
+    [Export] private NodePath _toolsListNodePath;
 
     #endregion
 
     #region Class Implementation
 
-    private void InitToolsPanels() {
-        GD.Print("InitToolsPanels 1");
+    public void InitToolsPanels() {
         if (this.TryGetCached(ref _toolsContainer, _toolsContainerPath)) {
-            GD.Print("InitToolsPanels 2");
             EditorToolsController.instance.tools.ForEach(t => {
                 var panel = GD.Load<PackedScene>(t.panelPath).Instantiate();
-                GD.Print($"InitToolsPanels 3 -> {panel}");
                 _toolsContainer.AddChild(panel);
             });
+        }
+        
+        if (this.TryGetCached(ref _toolsListNode, _toolsListNodePath)) {
+            _toolsListNode.SetSize(new Vector2(_toolsListNode.Size.X, _defaultPanelHeight));
+        }
+    }
+
+    public void IncreaseSize() {
+        if (this.TryGetCached(ref _toolsListNode, _toolsListNodePath)) {
+            _panelHeightDelta = Mathf.Min(3000, _panelHeightDelta + 10);
+            _toolsListNode.SetSize(new Vector2(_toolsListNode.Size.X, _defaultPanelHeight + _panelHeightDelta));
+        }
+    }
+
+    public void DecreaseSize() {
+        if (this.TryGetCached(ref _toolsListNode, _toolsListNodePath)) {
+            _panelHeightDelta = Mathf.Max(-600, _panelHeightDelta - 10);
+            _toolsListNode.SetSize(new Vector2(_toolsListNode.Size.X, _defaultPanelHeight + _panelHeightDelta));
         }
     }
 
