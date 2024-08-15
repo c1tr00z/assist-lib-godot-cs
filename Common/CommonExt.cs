2a1bae8cb1d0cd5af07775f7a91a773cfc92083f
c1tr00z <c1tr00z@yandex.ru>
2024-08-15 23:22:35 +0900

    DecimalExt class

--- Common/CommonExt.cs
+++ Common/CommonExt.cs
@@ -4,6 +4,7 @@ using Godot;
 namespace c1tr00z.AssistLib.Common;
 
 public static class CommonExt {
+    
     #region Class Implementation
 
     public static T GetCached<T>(ref T cached, Func<T> getter) {
