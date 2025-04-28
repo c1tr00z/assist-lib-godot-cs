5be9c6cdfb009fbb203c4d8228c38b808f6087b3
c1tr00z <c1tr00z@yandex.ru>
2025-04-28 22:26:03 +0900

    DB Editor Tool reworked

--- DB/Editor/EditorTool/AssistLibEditorToolDBPanel.cs
+++ DB/Editor/EditorTool/AssistLibEditorToolDBPanel.cs
@@ -1,16 +1,32 @@
+using AssistLib.DB.Runtime;
 using Godot;
+using projectwitch.addons.AssistLib.EditorTools.Scripts;
 
 namespace AssistLib.DB.Editor.EditorTool;
 
 [Tool]
 [GlobalClass]
-public partial class AssistLibEditorToolDBPanel : VBoxContainer {
+public partial class AssistLibEditorToolDBPanel : EditorToolPanelRuntime<EditorToolDB> {
+    
+    #region EditorToolPanelRuntime Implementation
+
+    protected override void OnRequestData() { }
+    
+    protected override void OnToolLoaded(AssistLibEditorTool tool) { }
+
+    protected override void BuildPanelWidgets() {
+        AddChild(EditorToolsUI.MakeButton("Collect DB Entries", CollectDBEntries));
+        AddChild(EditorToolsUI.MakeButton("Force reload", ForceReload));
+    }
+
+    #endregion
 
     #region Class Implementation
 
     public void CollectDBEntries() {
 #if TOOLS
         DBEditorActions.CollectDBEntries();
+        
 #endif
     }
     
